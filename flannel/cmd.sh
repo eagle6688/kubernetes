a80ab0b548ed8c3c11c73fc1ff7429cfa439d47f
Aldwin Su <fangyansu@163.com>
2018-01-18 17:21:45 +0800

    update

--- flannel/cmd.sh
+++ flannel/cmd.sh
@@ -1,7 +1,19 @@
-#Step 1. open ports 8285
+#Add configuration in etcd
+etcdctl put /coreos.com/network/config '{ "Network": "172.17.0.0/16" }'
+curl -X PUT http://127.0.0.1:2379/v2/keys/coreos.com/network/config -d value='{ "Network": "172.17.0.0/16" }'
+
+#Open ports 8285
 sudo firewall-cmd --zone=public --add-port=8285/tcp --permanent
 sudo firewall-cmd --reload
 sudo firewall-cmd --list-ports
 
 #Create log directory
 sudo mkdir -p /var/log/flannel
+
+#replace flanneld
+sudo cp -f flanneld.4450 /etc/sysconfig/flanneld
+
+#start
+sudo systemctl daemon-reload
+sudo systemctl start flanneld
+sudo systemctl status flanneld
