ee5810ea044eabac7eee90da06250134f09ce0af
Aldwin Su <fangyansu@163.com>
2018-01-21 18:25:00 +0800

    update

--- kubernetes/install-master.sh
+++ kubernetes/install-master.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+dir_ln=/usr/local/bin
 dir_intall=/usr/local/programes/kubernates
 dir_client=$dir_intall/client
 dir_server=$dir_intall/server
@@ -17,6 +18,41 @@ function check_file(){
     return 0;
 }
 
+function create_dir(){
+    local -r dir="$1"
+
+    if [ ! -d "$dir" ]
+    then
+        mkdir -p "$dir"
+        echo "Create directory $dir"
+    fi
+}
+
+function extract_file(){
+    local -r file="$1"
+    local -r targetDir="$2"
+    local -r gzDeep=$3
+    
+    create_dir $targetDir
+    tar -xzf "${file}" --strip-components $gzDeep -C "${targetDir}"
+    echo "Extract $file to $targetPath"
+}
+
+function ln_file(){
+    local -r file="$1"
+    local -r target="$2"
+    ln -sf "$file" "$target"
+    echo "Create a soft link from $file to $target"
+}
+
+function executable_file(){
+    local -r file="$1"
+    chmod +x $file
+    echo "Make $file executable"
+}
+
+# Check files
+
 check_file "$client_file"
 check_client_result=$?
 
@@ -28,19 +64,11 @@ then
     exit 1;
 fi
 
+# Check files end
+
 echo "We will install kubernates in ${dir_intall}"
 
-if [ ! -d "$dir_intall" ]
-then
-    mkdir -p "$dir_intall"
-fi
-
-if [ ! -d "$dir_client" ]
-then
-    mkdir -p "$dir_client"
-fi
-
-if [ ! -d "$dir_server" ]
-then
-    mkdir -p "$dir_server"
-fi
+# Install client
+extract_file $client_file $dir_client 3
+executable_file "${dir_client}/kubectl"
+ln_file "${dir_client}/kubectl" "${dir_ln}/kubectl"
