6c25ef5e5931a1880726d91c914ba03485794d13
Aldwin Su <fangyansu@163.com>
2018-01-22 17:08:41 +0800

    update

--- kubernetes/apiserver/install.sh
+++ kubernetes/apiserver/install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-. /home/service/devutility.shell/tools.sh
+. ./apiserver.4450
 
-volume_srvkube=/srv/kubernetes
-volume_etcssl=/etc/ssl
-
-create_dir $volume_srvkube
-create_dir $volume_etcssl
-
-kubectl create -f apiserver.json
+kubectl run kube-apiserver \
+ --image=eagle6688/kube-apiserver:v1.9.2 \
+ --port=8080 \
+ --replicas=2 \
+ --command=true \
+ -- kube-apiserver \
+ $KUBE_API_ADDRESS \
+ $KUBE_API_PORT \
+ $KUBELET_PORT \
+ $KUBE_ETCD_SERVERS \
+ $KUBE_SERVICE_ADDRESSES \
+ $KUBE_ADMISSION_CONTROL
