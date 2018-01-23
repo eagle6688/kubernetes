a09d30404e7089774812839f27a499dabb0a0bc3
Aldwin Su <fangyansu@163.com>
2018-01-23 17:59:17 +0800

    update

--- kubernetes/apiserver/main.sh
+++ kubernetes/apiserver/main.sh
@@ -18,7 +18,12 @@ $dir_kube/server/kube-apiserver \
  $KUBE_SERVICE_ADDRESSES \
  $KUBE_ADMISSION_CONTROL
 
- /usr/local/programes/kubernetes/server/kube-apiserver \
-  --token-auth-file=/dev/null \
-  --insecure-bind-address=127.0.0.1
-  --advertise-address=
+/usr/local/programes/kubernetes/server/kube-apiserver \
+ --token-auth-file=/dev/null \
+ --insecure-bind-address=0.0.0.0 \
+ --insecure-port=8080 \
+ --advertise-address=10.100.97.236 
+ --secure-port=0 \
+ --etcd-servers=http://10.100.97.236:2379,http://10.100.97.92:2379,http://10.100.97.81:2379 \
+ --service-cluster-ip-range=10.254.0.0/16 \
+ --admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,ResourceQuota
